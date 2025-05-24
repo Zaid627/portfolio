d3fa38a2fc16375d1289551a6c81b9a1aedacfde
Muhammad Zaid <muhammadzaid@Muhammads-MacBook-Air.local>
2025-05-24 19:49:15 +0500

    commit 5

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,22 @@
 //   plugins: [react(), tailwindcss()],
 // })
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: './postcss.config.js',
+  },
+});
 
