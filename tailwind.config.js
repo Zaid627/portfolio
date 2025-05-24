a8e3f78787bb9a5ed2ab4ff41eebbdb29838b83c
Muhammad Zaid <muhammadzaid@Muhammads-MacBook-Air.local>
2025-05-24 18:29:28 +0500

    commit one

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './index.html',
+//     './src/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
