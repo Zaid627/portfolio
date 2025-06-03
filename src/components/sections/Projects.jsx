3801899de8e281c3b8fa744f8a51b6021791f43a
Muhammad Zaid <muhammadzaid@Muhammads-MacBook-Air.local>
2025-06-03 14:06:44 +0500

    add1

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -113,6 +113,32 @@ export const Project = () =>{
                     </div>
                     {/* 5th */}
 
+                     <div className="p-6 rounded-xl border border-white/10 
+                    hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                        <h3 className="text-xl font-bold mb-2">Ecommerce App</h3>
+                        <p className="text-gray-400 md-4">This is ecommerce nextJS MERN STACK project
+                            user place order and do payment by card and we get payment through stripe. 
+                            The project Tools mention blow. 
+                        </p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["Next.JS ,","Node.JS ,","MongoDB ,","Stripe ,"
+                                ,"Tailwindcss ,"].map((tech, key)=>(
+                                    <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 
+                                    px-1 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                                        {tech}
+
+                                    </span>
+                                ))}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <a href="https://ecommerce-mini-xi.vercel.app/" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project </a>
+                        </div>
+                    </div>
+
+
                     
 
 
